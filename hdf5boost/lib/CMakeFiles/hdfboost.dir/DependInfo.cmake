74045ae5a7cfe2d591e61327cdd48c1a86a42369
philipp <philipp.pelz@mpsd.mpg.de>
2015-05-12 16:10:56 +0200

    cmake

---
+++ hdf5boost/lib/CMakeFiles/hdfboost.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/philipp/projects/QSTEM/hdf5boost/lib/HDFFile.cpp" "/home/philipp/projects/QSTEM/hdf5boost/lib/CMakeFiles/hdfboost.dir/HDFFile.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "BOOST_LOG_DYN_LINK"
+  "USE_THREADS"
+  "VERSION=\"3.0\""
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "hdf5boost/lib"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
